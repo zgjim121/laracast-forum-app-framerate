4c1a11f1e551521399edba73ab6c869defb88cdd
zgjim neziraj <zgjimneziraj@zgjims-MacBook-Air.local>
2025-04-14 18:32:27 +0200

    Initial commit

---
+++ resources/js/Components/ApplicationMark.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="/assets/symbol-analog.png" alt="">
+</template>
---
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Link :href="'/'">
+        <img src="/assets/symbol-analog.png" alt="">
+    </Link>
+</template>

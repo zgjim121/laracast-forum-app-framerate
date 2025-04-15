25f7b879e0752b1fe0ce13ab91e5289b7e9dc469
zgjim neziraj <zgjimneziraj@zgjims-MacBook-Air.local>
2025-04-15 11:17:24 +0200

    Factories and Seeding the Database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+        $posts = Post::factory(200)
+            ->recycle($users)
+            ->create();
+
+        $comments = Comment::factory(100)->recycle($users)->recycle($posts)->create();
+
+
+        $zgjim = User::factory()
+            ->has(Post::factory(20))
+            ->has(Comment::factory(100)->recycle($posts))
+            ->create([
+                'name' => 'zgjim',
+                'email' => 'test@example.com',
+            ]);
     }
 }

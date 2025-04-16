7abe38029c296b7e0a957050392fa51c96a44a56
zgjim neziraj <zgjimneziraj@zgjims-MacBook-Air.local>
2025-04-16 11:54:39 +0200

    from model to json

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => UserResource::make($this->user),
+            'post' => PostResource::make($this->post),
+            'body' => $this->body,
+            'updated_at' => $this->updatedAt,
+            'created_at' => $this->createdAt,
+        ];
+    }
+}

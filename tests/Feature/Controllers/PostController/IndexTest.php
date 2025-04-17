d520a5abc21f44d165f99cb7ec2c21fda940596e
zgjim neziraj <zgjimneziraj@zgjims-MacBook-Air.local>
2025-04-17 02:39:14 +0200

    Helpful Test Helpers

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 use Inertia\Testing\AssertableInertia;
 use function Pest\Laravel\get;
 
@@ -11,8 +15,10 @@ it('should return the correct component', function () {
 });
 
 it('passes posts to the view', function () {
+
+    $posts = Post::factory(3)->create();
+
     get(route('posts.index'))
-        ->assertInertia(fn(AssertableInertia $inertia) => $inertia
-            ->has('posts')
-        );
+        ->assertHasPaginatedResource('posts', PostResource::collection($posts->reverse()));
 });
+

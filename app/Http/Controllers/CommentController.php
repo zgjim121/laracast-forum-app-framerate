8d33e0e4a21946400825ab2eb5802b1765cb211e
zgjim neziraj <zgjimneziraj@zgjims-MacBook-Air.local>
2025-04-26 02:13:22 +0200

    making use of the banner method for displaying messages when we made a change ex. adding, edit or delete comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -24,7 +24,8 @@ class CommentController extends Controller
             'user_id' => $request->user()->id,
         ]);
 
-        return to_route('posts.show', $post);
+        return to_route('posts.show', $post)
+            ->banner('Comment Added.');
     }
 
     /**
@@ -38,7 +39,8 @@ class CommentController extends Controller
 
         $comment->update($data);
 
-        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')]);
+        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')])
+            ->banner('Comment Updated.');
     }
 
     /**
@@ -50,6 +52,7 @@ class CommentController extends Controller
 
         $comment->delete();
 
-        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')]);
+        return to_route('posts.show', ['post' => $comment->post_id, 'page' => $request->query('page')])
+            ->banner('Comment Deleted.');
     }
 }
